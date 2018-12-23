7785e3f3dce5ff322e070e8b81cb197f483f92ba
ddaninthe <d.daninthelopez@gmail.com>
2018-12-23 14:09:08 +0100

    Donkey update

--- Donkey.cpp
+++ Donkey.cpp
@@ -2,8 +2,20 @@
 #include "Donkey.h"
 
 
-Donkey::Donkey(const sf::Texture& texture, sf::Vector2f position) : Entity(texture, position, EntityType::donkey)
+Donkey::Donkey(const sf::Texture& texture, sf::Vector2f position) : mOtherTextures(), Entity(texture, position, EntityType::donkey)
 {
+	mOtherTextures.push_back(sf::Texture());
+	mOtherTextures.back().loadFromFile("Media/Textures/dk_throw_right.png");
+	mOtherTextures.push_back(sf::Texture());
+	mOtherTextures.back().loadFromFile("Media/Textures/dk_barrel.png");
+	mOtherTextures.push_back(sf::Texture());
+	mOtherTextures.back().loadFromFile("Media/Textures/dk_throw_left.png");
+	mOtherTextures.push_back(sf::Texture());
+	mOtherTextures.back().loadFromFile("Media/Textures/dk_happy_1.png");
+	mOtherTextures.push_back(sf::Texture());
+	mOtherTextures.back().loadFromFile("Media/Textures/dk_happy_2.png");
+	mOtherTextures.push_back(sf::Texture());
+	mOtherTextures.back().loadFromFile("Media/Textures/dk_loss.png");
 }
 
 
--- Donkey.h
+++ Donkey.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "Entity.h"
 
+using namespace std;
+
 class Donkey :
 	public Entity
 {
 public:
 	Donkey(const sf::Texture&, sf::Vector2f);
 	~Donkey();
+
+public:
+	vector<sf::Texture> mOtherTextures;
 };
