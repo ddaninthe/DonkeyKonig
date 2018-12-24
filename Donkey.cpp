2b6ff2d194adcffb1fe4504a42c8cdd73bb1cbef
ddaninthe <d.daninthelopez@gmail.com>
2018-12-24 14:01:28 +0100

    Added Donkey animation

--- Donkey.cpp
+++ Donkey.cpp
@@ -2,7 +2,9 @@
 #include "Donkey.h"
 
 
-Donkey::Donkey(const sf::Texture& texture, sf::Vector2f position) : Entity(texture, position, EntityType::donkey)
+Donkey::Donkey(const sf::Texture& texture, sf::Vector2f position) :
+	mThrowBarrel(false), mAnimationState(DonkeyStates::front), mAnimationTime(0)
+	, Entity(texture, position, EntityType::donkey)
 {
 	mTextures.push_back(make_shared<sf::Texture>());
 	mTextures.back()->loadFromFile("Media/Textures/dk_throw_right.png");
@@ -21,4 +23,36 @@ Donkey::Donkey(const sf::Texture& texture, sf::Vector2f position) : Entity(textu
 
 Donkey::~Donkey()
 {
+}
+
+void Donkey::updateAnimation(sf::Time elapsedTime) {
+	mAnimationTime += elapsedTime.asMilliseconds();
+
+	if (mAnimationTime > TIME_PER_ANIMATION) {
+		if (mAnimationState == DonkeyStates::throwLeft) {
+			mAnimationState = DonkeyStates::front;
+			mAnimationTime = 0;
+		}
+		else {
+			mAnimationState++;
+		}
+		mAnimationTime -= TIME_PER_ANIMATION;
+	}
+
+	mSprite.setTexture(*mTextures.at(mAnimationState));
+
+	if (mAnimationState == DonkeyStates::throwLeft) {
+		if (mThrowBarrel) {
+			addBarrel();
+		}
+	}
+	else {
+		mThrowBarrel = true;
+	}
+}
+
+void Donkey::addBarrel() {
+	mThrowBarrel = false;
+	cout << "Roll Roll Roll" << endl;
+	// TODO
 }
