fba37bd716fe34032868acf6904d338c9f0478a9
Thomas Gianfalla <44254960+Her0iik@users.noreply.github.com>
2019-02-23 17:15:51 +0100

    try to add sound

--- Game.cpp
+++ Game.cpp
@@ -3,37 +3,47 @@
 #include "Game.h"
 #include "EntityManager.h"
 
+#include <SFML/Audio.hpp>
+
+
 const float Game::PlayerSpeed = 100.f;
 const sf::Time Game::TimePerFrame = sf::seconds(1.f / 60.f);
+int Game::Score;
+int Game::Life;
+bool Game::GameFinished;
+bool Game::Restart;
 
 Game::Game()
 	: mWindow(sf::VideoMode(840, 600), "Donkey Kong 1981", sf::Style::Close)
 	, mFont()
 	, mStatisticsText()
+	, mScoreText()
+	, mLifeText()
+	, mGameText()
+	, mCheatCode()
+	, mRetryText()
 	, mStatisticsUpdateTime()
 	, mStatisticsNumFrames(0)
-	, mIsMovingUp(false)
-	, mIsMovingDown(false)
-	, mIsMovingRight(false)
-	, mIsMovingLeft(false)
+	, mInput("")
 {
 	mWindow.setFramerateLimit(160);
 
-	sf::Texture test;
+	// Initialize static values
+	Game::Score = 0;
+	Game::Life = 3;
+	Game::GameFinished = false;
+	Game::Restart = false;
 
 	// Draw blocks
 
-	_TextureBlock.loadFromFile("Media/Textures/Block.png");
-	_sizeBlock = _TextureBlock.getSize();
-
+	sf::Vector2f position;
 	for (int i = 0; i < BLOCK_COUNT_X; i++)
 	{
 		for (int j = 0; j < BLOCK_COUNT_Y; j++)
 		{
-			_Block[i][j].setTexture(_TextureBlock);
-			if (j == 0) {
+			if (j == 0 && i != 0) {
 				// Upper plateform
-				_Block[i][j].setPosition(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1));
+				position = sf::Vector2f(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1));
 				
 			} else if (j % 2 == 0) {
 				// Don't draw first block
@@ -41,105 +51,109 @@ Game::Game()
 					continue;
 				}
 
-				_Block[i][j].setPosition(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1) - i);
+				position = sf::Vector2f(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1) - i);
 			}
 			else {
 				// Don't draw last block
 				if (i == BLOCK_COUNT_X - 1) {
 					continue;
 				}
-				_Block[i][j].setPosition(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1) + i);
+				position = sf::Vector2f(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (j + 1) + i);
 			}
 
-			std::shared_ptr<Entity> se = std::make_shared<Entity>();
-			se->mSprite = _Block[i][j];
-			se->mType = EntityType::block;
-			se->mSize = _TextureBlock.getSize();
-			EntityManager::mEntities.push_back(se);
+			EntityManager::addEntity(EntityType::block, position);
 		}
 	}
 
 	// Draw Echelles
-
-	_TextureEchelle.loadFromFile("Media/Textures/Echelle.png");
-
-	for (int i = 0; i < ECHELLE_COUNT; i++)
+	float sizeBlock = EntityManager::getBlocks().front()->mSprite.getGlobalBounds().height;
+	for (int i = 0; i < LADDER_COUNT; i++)
 	{
-		_Echelle[i].setTexture(_TextureEchelle);
-		_Echelle[i].setPosition(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (i + 1) + _sizeBlock.y);
-
-		std::shared_ptr<Entity> se = std::make_shared<Entity>();
-		se->mSprite = _Echelle[i];
-		se->mType = EntityType::echelle;
-		se->mSize = _TextureEchelle.getSize();
-		EntityManager::mEntities.push_back(se);
+		sf::Vector2f position(110.f + 70.f * (i % 2 == 0 ? 2 : BLOCK_COUNT_X - 1), 0.f + BLOCK_SPACE * (i + 1) + sizeBlock);
+		EntityManager::addEntity(EntityType::ladder, position);
 	}
 
-
-	// Draw Mario
-	shared_ptr<Entity> player = make_shared<Entity>();
-	player->mTexture = sf::Texture();
-
-	sf::Sprite spriteMario;
-	sf::Vector2f posMario;
-
-	player->mTexture.loadFromFile("Media/Textures/mario_alt.png"); // Mario_small.png");
-	spriteMario.setTexture(player->mTexture);
-
-	posMario.x = 100.f + 70.f;
-	posMario.y = BLOCK_SPACE * 5 - player->mTexture.getSize().y;
-
-	spriteMario.setPosition(posMario);
-
-	player->mSprite = spriteMario;
-	player->mType = EntityType::player;
-	player->mSize = player->mTexture.getSize();
-	EntityManager::mEntities.push_back(player);
-
-
-	// Draw Donkey Kong
-	_TextureDonkey[0].loadFromFile("Media/Textures/dk.png");
-	_TextureDonkey[1].loadFromFile("Media/Textures/dk_barrel.png");
-	_TextureDonkey[2].loadFromFile("Media/Textures/dk_throw_left.png");
-	_TextureDonkey[3].loadFromFile("Media/Textures/dk_throw_right.png");
-	_TextureDonkey[4].loadFromFile("Media/Textures/dk_happy_1.png");
-	_TextureDonkey[5].loadFromFile("Media/Textures/dk_happy_2.png");
-	_TextureDonkey[6].loadFromFile("Media/Textures/dk_loss.png");
-
-	dkTexture.loadFromFile("Media/Textures/dk.png");;
-	_sizeDk = dkTexture.getSize();
-	_DonkeyKong.setTexture(dkTexture);
-	sf::Vector2f posDk;
-	posDk.x = 580.f;
-	posDk.y = BLOCK_SPACE * 1 - _sizeDk.y;
-
-	_DonkeyKong.setPosition(posDk);
-
-	std::shared_ptr<Entity> dk = std::make_shared<Entity>();
-	dk->mSprite = _DonkeyKong;
-	dk->mType = EntityType::donkey;
-	dk->mSize = dkTexture.getSize();
-	EntityManager::mEntities.push_back(dk);
+	//Draw Coins
+	for (int i = 0; i < COIN_COUNT; i++)
+	{
+		sf::Vector2f position(110.f + 70.f * (i % 2 == 0 ? 3 : BLOCK_COUNT_X - 2), 30.f + BLOCK_SPACE * (i + 1) + sizeBlock);
+		EntityManager::addEntity(EntityType::coin, position);
+	}
 	
+	// Draw Mario
+	EntityManager::addEntity(EntityType::player);
 
 	// Draw Lady
-	// TODO
+	EntityManager::addEntity(EntityType::lady);
 
+	// Draw Donkey Kong
+	EntityManager::addEntity(EntityType::donkey);
 
-	// Initialize barrels
-	_TextureBarrel[0].loadFromFile("Media/Textures/barrel_front.png");
-	for (int i = 1; i < 5; i++) {
-		_TextureBarrel[i].loadFromFile("Media/Textures/barrel_rolling_" + to_string(i) + ".png");
+	// Draw barrels
+	for (int i = 0; i < MAX_BARREL; i++) {
+		EntityManager::addEntity(EntityType::barrel);
 	}
-	
 
-	// Draw Statistic Font 
+
+	// Standing barrel
+	sf::Texture texture;
+	texture.loadFromFile("Media/Textures/barrel_standing.png");
+	position = sf::Vector2f(610.f, BLOCK_SPACE - texture.getSize().y);
+	decorations.push_back(make_shared<Entity>(texture, position, EntityType::barrel));
+
+	// Barrel Destructor
+	sf::Texture textureDestructor;
+	textureDestructor.loadFromFile("Media/Textures/barrel_destructor.png");
+	position = sf::Vector2f(170.f, BLOCK_SPACE * 5 - textureDestructor.getSize().y);
+	EntityManager::barrelDestructor = make_shared<Entity>(textureDestructor, position, EntityType::barrel);
+
 
 	mFont.loadFromFile("Media/Sansation.ttf");
+	// Draw Statistic Font 
 	mStatisticsText.setString("Welcome to Donkey Kong 1981");
 	mStatisticsText.setFont(mFont);
 	mStatisticsText.setPosition(5.f, 5.f);
 	mStatisticsText.setCharacterSize(10);
+
+	// Draw Score
+	mScoreText.setString("Score: 0");
+	mScoreText.setFont(mFont);
+	mScoreText.setPosition(210.f, 10.f);
+	mScoreText.setCharacterSize(30);
+
+	// Draw Life
+	mLifeText.setString("Life: 3");
+	mLifeText.setFont(mFont);
+	mLifeText.setPosition(365.f, 10.f);
+	mLifeText.setCharacterSize(30);
+
+	// Cheat code ON or OFF
+	mCheatCode.setString("Cheat Code OFF");
+	mCheatCode.setFont(mFont);
+	mCheatCode.setPosition(620.f, 20.f);
+	mCheatCode.setCharacterSize(15);
+	mCheatCode.setFillColor(sf::Color(255, 16, 0));
+
+	// Victory or Game Over
+	mGameText.setFont(mFont);
+	mGameText.setPosition(230.f, 230.f);
+	mGameText.setCharacterSize(100);
+	mGameText.setFillColor(sf::Color(255, 16, 0));
+
+	// Retry Game
+	mRetryText.setFont(mFont);
+	mRetryText.setPosition(300.f, 400.f);
+	mRetryText.setCharacterSize(30);
+
+	// Sound
+	sf::SoundBuffer buffer;
+	buffer.loadFromFile("Media/Sounds/Music.wav");
+	sf::Sound sound;
+	sound.setBuffer(buffer);
+	sound.setLoop(true);
+	sound.setVolume(50);
+	sound.play();
+	
 }
 
 void Game::run()
@@ -165,19 +179,74 @@ void Game::run()
 
 void Game::processEvents()
 {
+	const sf::Time ellapsedTime;
+
 	sf::Event event;
 	while (mWindow.pollEvent(event))
 	{
+		shared_ptr<Mario> player = EntityManager::getPlayer();
 		switch (event.type)
 		{
-		case sf::Event::KeyPressed:
-			handlePlayerInput(event.key.code, true);
-			break;
+		case sf::Event::KeyPressed:				
+			switch (event.key.code)
+			{
+			case sf::Keyboard::M: 
+				mInput += 'M';
+				break;
+			case sf::Keyboard::A:
+				if (mInput.length() == 1) {
+					mInput += 'A';
+				}
+				else mInput = "";
+				break;
+			case sf::Keyboard::R:
+				if (mInput.length() == 2) {
+					mInput += 'A';
+				}
+				else mInput = "";
+				break;				
+			case sf::Keyboard::I:
+				if (mInput.length() == 3) {
+					mInput += 'A';
+				}
+				else mInput = "";
+				break;				
+			case sf::Keyboard::O:
+				
+				if (mInput.length() == 4) {
+					if (EntityManager::cheatCodeEnabled == false) {
+						EntityManager::cheatCodeEnabled = true;
+						mCheatCode.setFillColor(sf::Color(17, 255, 73));
+						mCheatCode.setString("Cheat Code ON");
+					}
+					else {
+						EntityManager::cheatCodeEnabled = false;
+						mCheatCode.setFillColor(sf::Color(255, 16, 0));
+						mCheatCode.setString("Cheat Code OFF");
 
+					}
+					mInput = "";
+				}
+				else mInput = "";
+				break;
+			case sf::Keyboard::Enter:
+				if (isFinished()) {
+					Game::Restart = true;
+					mWindow.close();
+				}
+				break;
+			default:
+				mInput = "";
+			}
+			if (Score >= 0) {
+				player->handlePlayerInput(event.key.code, true);
+			}
+			break;
 		case sf::Event::KeyReleased:
-			handlePlayerInput(event.key.code, false);
+			if (Score >= 0) {
+				player->handlePlayerInput(event.key.code, false);
+			}
 			break;
-
 		case sf::Event::Closed:
 			mWindow.close();
 			break;
@@ -187,65 +256,25 @@ void Game::processEvents()
 
 void Game::update(sf::Time elapsedTime)
 {
-	sf::Vector2f playerPosition = EntityManager::GetPlayer()->mSprite.getPosition();
-
-	sf::Vector2f movement(0.f, 0.f);
-	if (mIsMovingUp) {
-		movement.y -= PlayerSpeed;
-	}
-	if (mIsMovingDown) {
-		movement.y += PlayerSpeed;
-	}
-	if (mIsMovingLeft) {
-		// Avoid exiting from screen
-		if (playerPosition.x < MIN_X) {
-			return;
-		}
-		else {
-			movement.x -= PlayerSpeed;
-		}
-	}
-	if (mIsMovingRight) {
-		// Avoid exiting from screen
-		if (playerPosition.x > MAX_X) {
-			return;
-		}
-		else {
-			movement.x += PlayerSpeed;
-		}
-	}
-
-	for (std::shared_ptr<Entity> entity : EntityManager::mEntities)
-	{
-		if (entity->mEnabled == false)
-		{
-			continue;
-		}
-
-		if (entity->mType != EntityType::player)
-		{
-			continue;
-		}
-
-		entity->mSprite.move(movement * elapsedTime.asSeconds());
-	}
+	EntityManager::updateMovingEntities(elapsedTime);
 }
 
 void Game::render()
 {
 	mWindow.clear();
 
-	for (std::shared_ptr<Entity> entity : EntityManager::mEntities)
-	{
-		if (entity->mEnabled == false)
-		{
-			continue;
-		}
-
-		mWindow.draw(entity->mSprite);
+	for (shared_ptr<Entity> decoration : decorations) {
+		mWindow.draw(decoration->mSprite);
 	}
 
+	EntityManager::draw(mWindow);
+
 	mWindow.draw(mStatisticsText);
+	mWindow.draw(mScoreText);
+	mWindow.draw(mLifeText);
+	mWindow.draw(mGameText);
+	mWindow.draw(mCheatCode);
+	mWindow.draw(mRetryText);
 	mWindow.display();
 }
 
@@ -253,6 +282,23 @@ void Game::updateStatistics(sf::Time elapsedTime)
 {
 	mStatisticsUpdateTime += elapsedTime;
 	mStatisticsNumFrames += 1;
+
+	if (Game::Life == 0) {
+		mGameText.setString("GAME OVER");
+		mLifeText.setString("Life: 0");
+		mRetryText.setString("Press 'Enter' to Retry");
+		GameFinished = true;
+	}
+	else if (Score >= 100000) {
+		mGameText.setString("VICTORY");
+		mRetryText.setString("Press 'Enter' to Retry");
+		mGameText.setFillColor(sf::Color(17, 255, 73));
+		GameFinished = true;
+	}
+	else {
+		mScoreText.setString("Score: " + to_string(Game::Score));
+		mLifeText.setString("Life: " + to_string(Game::Life));
+	}
 
 	if (mStatisticsUpdateTime >= sf::seconds(1.0f))
 	{
@@ -263,40 +309,8 @@ void Game::updateStatistics(sf::Time elapsedTime)
 		mStatisticsUpdateTime -= sf::seconds(1.0f);
 		mStatisticsNumFrames = 0;
 	}
-
-	//
-	// Handle collision
-	//
-
-	if (mStatisticsUpdateTime >= sf::seconds(0.050f))
-	{
-		// Handle collision weapon enemies
-	}
 }
 
-void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
-{
-	if (key == sf::Keyboard::Up)
-		mIsMovingUp = isPressed;
-	else if (key == sf::Keyboard::Down)
-		mIsMovingDown = isPressed;
-	else if (key == sf::Keyboard::Left)
-		mIsMovingLeft = isPressed;
-	else if (key == sf::Keyboard::Right)
-		mIsMovingRight = isPressed;
-
-	if (key == sf::Keyboard::Space)
-	{
-
-	}
-}
-
-void Game::addBarrel() {
-	// TODO
-	/*
-	sf::Sprite sprite;
-	sprite.setTexture(_TextureBarrel[1]);
-	sprite.setPosition(100.f, 100.f);
-
-	_Barrels.push_back(sprite);*/
+bool Game::isFinished() {
+	return GameFinished;
 }
